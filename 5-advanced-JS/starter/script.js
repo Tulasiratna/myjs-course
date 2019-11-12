f1ad91543e79a36568456d9cc54afe74881703ed
Tulasi ratna <ratna.tulasi341@gmail.com>
2019-11-12 12:01:02 +0100

    inherited calculateAge function

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -8,15 +8,15 @@ var Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
-  this.calculateAge = function() {
-    console.log(2019 - this.yearOfBirth);
-  }
 }
 
+Person.prototype.calculateAge = function() {
+    console.log(2019 - this.yearOfBirth);
+  }
+
 var jonas = new Person('Jonas', 1989, 'engineer');
-
-jonas.calculateAge();
-
 var divith = new Person('Divith', 2017, 'still student');
 var soumya = new Person('Soumya', 1986, 'designer');
-//divith.calculateAge();
+jonas.calculateAge();
+divith.calculateAge();
+soumya.calculateAge();
