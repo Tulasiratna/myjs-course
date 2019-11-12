19dc47ba55542eed83df2c13c7986c0545a8b7b7
Tulasi ratna <ratna.tulasi341@gmail.com>
2019-11-12 11:56:01 +0100

    new objects added

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -2,4 +2,21 @@ var jonas = {
   name: 'Jonas',
   yearOfBirth: 1989,
   job: 'engineer'
+};
+
+var Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+  this.calculateAge = function() {
+    console.log(2019 - this.yearOfBirth);
+  }
 }
+
+var jonas = new Person('Jonas', 1989, 'engineer');
+
+jonas.calculateAge();
+
+var divith = new Person('Divith', 2017, 'still student');
+var soumya = new Person('Soumya', 1986, 'designer');
+//divith.calculateAge();
