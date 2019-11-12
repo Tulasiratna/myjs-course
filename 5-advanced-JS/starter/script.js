b8b4507d814d557c8750dc7226d33d7bbca31292
Tulasi ratna <ratna.tulasi341@gmail.com>
2019-11-12 12:06:44 +0100

    lastName inherited

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -13,6 +13,7 @@ var Person = function(name, yearOfBirth, job) {
 Person.prototype.calculateAge = function() {
     console.log(2019 - this.yearOfBirth);
   }
+Person.prototype.lastName = 'Reddy';
 
 var jonas = new Person('Jonas', 1989, 'engineer');
 var divith = new Person('Divith', 2017, 'still student');
@@ -20,3 +21,8 @@ var soumya = new Person('Soumya', 1986, 'designer');
 jonas.calculateAge();
 divith.calculateAge();
 soumya.calculateAge();
+
+console.log(jonas.lastName);
+console.log(divith.lastName);
+console.log(soumya.lastName);
+
