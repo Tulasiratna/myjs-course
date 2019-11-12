4f0a32a546927e0a442055c2dcf8762a7193eec0
Tulasi ratna <ratna.tulasi341@gmail.com>
2019-11-12 13:39:43 +0100

    objects created

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,8 +1,10 @@
+/*
 var jonas = {
   name: 'Jonas',
   yearOfBirth: 1989,
   job: 'engineer'
 };
+// function constructor (alwayas starts with capital letter (like Person))
 
 var Person = function(name, yearOfBirth, job) {
   this.name = name;
@@ -25,4 +27,21 @@ soumya.calculateAge();
 console.log(jonas.lastName);
 console.log(divith.lastName);
 console.log(soumya.lastName);
+*/
 
+var personProto = {
+  calculateAge: function() {
+    console.log(2019 - this.yearOfBirth);
+  }
+};
+
+var james = Object.create(personProto);
+james.name = 'James';
+james.yearOfBirth = 1992;
+james.job = 'designer';
+
+var sarah = Object.create(personProto, {
+  name: { value: 'Sarah'},
+  yearOfBirth: { value: 1988},
+  job: { value: 'engineer'}
+});
