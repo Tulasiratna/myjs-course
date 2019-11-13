a8d2e2d26e5ceb7fdd15dc1caef3094aa73863b4
Tulasi ratna <ratna.tulasi341@gmail.com>
2019-11-13 19:40:24 +0100

    still working on challenge -7

--- 5-advanced-JS/challenge-7/script.js
+++ 5-advanced-JS/challenge-7/script.js
@@ -19,6 +19,18 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
+
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
 function Question(question, answers, correct) {
   this.question = question;
   this.answers = answers;
@@ -32,14 +44,13 @@ Question.prototype.displayQuestion = function() {
       console.log(i + ': ' + this.answers[i]);
     }
   }
-Question.prototype.correctAnswer = function() {
+Question.prototype.correctAnswer = function(answer) {
   if(answer === this.correct) {
     console.log('Well done!, Correct answer');
   } else {
     console.log('wrong answer, better luck next time');
   }
 }
-
 
 var q1 = new Question('Is Udemy is best for learning code?',
                       ['Yes', 'No'],
@@ -53,9 +64,37 @@ var q3 = new Question('Which is the safest country in the world?',
 
 var questions = [q1, q2, q3];
 
-var question = [Math.floor(Math.random() * questions.length)];
+function score() {
+  var sc = 0;
+  return function(correct) {
+    if(correct) {
+      sc++;
+    }
+    return score;
+  }
+}
 
-questions[question].displayQuestion();
+var keepScore = score();
 
-var answer = parseInt(prompt('please select your answer'));
-questions[question].correctAnswer();
+function nextQuestion() {
+
+var randQuestion = [Math.floor(Math.random() * questions.length)];
+
+questions[randQuestion].displayQuestion();
+
+var answer = prompt('please select your answer');
+
+//questions[randQuestion].correctAnswer(answer);
+
+//nextQuestion();
+
+
+if(answer !== 'exit') {
+    questions[randQuestion].correctAnswer(parseInt(answer), keepScore);
+    nextQuestion();
+
+  }
+}
+
+nextQuestion();
+
