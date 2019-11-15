f0ff52d6a6907c54d0913a3e5682d69e1b1dd14d
Tulasi ratna <ratna.tulasi341@gmail.com>
2019-11-15 10:21:36 +0100

    DOMStrings added

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -7,6 +7,27 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
+
 
 
 })();
@@ -14,12 +35,26 @@ var UIController = (function() {
 // APP CONTROLLER
 var Controller = (function(bgtCtrl, UICtrl) {
 
+  var DOM = UICtrl.getDOMstrings;
+
+
 var addItem = function() {
 
+  // get the input field data
+  var input = UICtrl.getInput();
+  console.log(input);
 
-  // code
+  //add the item to the budget controller
 
-  console.log('just for testing');
+  // add the item to the UI
+
+  // budget calculation
+
+  //display budget on the UI
+
+
+
+  //console.log('just for testing');
 
 };
 
