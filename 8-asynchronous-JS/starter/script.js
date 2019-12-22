e9ea4008156954be252e4266c76ab1a412dc968b
Tulasi ratna <ratna.tulasi341@gmail.com>
2019-12-22 11:59:01 +0100

    started Ajax

--- 8-asynchronous-JS/starter/script.js
+++ 8-asynchronous-JS/starter/script.js
@@ -1,5 +1,14 @@
+const end = () => {
+  setTimeout(() => {
+    console.log('learning Ajax');
+  }, 2000);
+}
+
 const first = () => {
   console.log('hey!!!!');
+  end();
+  console.log("I'm here");
 }
 
 first();
+//end();
