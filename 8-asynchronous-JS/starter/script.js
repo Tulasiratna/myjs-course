77d89376249111c35aa057f89e76f2eed8702f0d
Tulasi ratna <ratna.tulasi341@gmail.com>
2019-12-23 16:06:27 +0100

    added title nad publisher

--- 8-asynchronous-JS/starter/script.js
+++ 8-asynchronous-JS/starter/script.js
@@ -1,14 +1,30 @@
-const end = () => {
-  setTimeout(() => {
-    console.log('learning Ajax');
+// const end = () => {
+//   setTimeout(() => {
+//     console.log('learning Ajax');
+//   }, 2000);
+// }
+
+// const first = () => {
+//   console.log('hey!!!!');
+//   end();
+//   console.log("I'm here");
+// }
+
+// first();
+//end();
+
+
+function getRecipe() {
+  setTimeout (() => {
+    const recipeID = [123, 234, 345, 456, 567];
+    console.log(recipeID);
+    setTimeout ((id) => {
+      const recipe = {
+        title: 'Kylling Kebab',
+        publisher: 'Tulasi'
+      };
+      console.log(`${id}: ${recipe.title}`);
+    }, 1000, recipeID[1]);
   }, 2000);
 }
-
-const first = () => {
-  console.log('hey!!!!');
-  end();
-  console.log("I'm here");
-}
-
-first();
-//end();
+getRecipe();
