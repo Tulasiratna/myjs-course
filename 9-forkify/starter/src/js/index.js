ef153945cb3ef81d4f5bee0921e0d2f6c84dce9e
Tulasi ratna <ratna.tulasi341@gmail.com>
2020-01-03 14:09:53 +0100

    added plugins and loader

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import x from './test';
-
-console.log(`I imported ${x} from another module called test.js`);
+const num = 45;
+console.log(`I imported ${x} from another module called test.js!! variable num is ${num}`);
--- 9-forkify/starter/webpack.config.js
+++ 9-forkify/starter/webpack.config.js
@@ -2,19 +2,33 @@ const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: ['babel-polyfill', './src/js/index.js'],
   output: {
     filename: 'js/bundle.js',
     path: path.resolve(__dirname, 'dist')
     //filename: './dist/js/bundle.js'
   },
+
   devServer: {
     contentBase: './dist'
   },
+
   plugins: [
     new htmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html'
     })
-  ]
+  ],
+
+  module: {
+    rules: [
+        {
+           test: /\.js$/,
+           exclude: /node_modules/,
+           use: {
+              loader: 'babel-loader'
+           }
+        }
+    ]
+  }
 };
