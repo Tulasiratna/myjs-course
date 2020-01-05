f07fd19904c12686e85e3fbf5f2cec460b3119f9
Tulasi ratna <ratna.tulasi341@gmail.com>
2020-01-05 17:17:19 +0100

    built search controller

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,6 +1,31 @@
 import Search from './models/Search';
 
-const search = new Search('Pizza');
-console.log(search);
-search.getResults();
+const state = {};
+
+const cntrlSearch = async () => {
+  // get query from view
+  const query = 'pizza';
+
+  if (query) {
+    //2. new search object and add it to state
+    state.search = new Search(query);
+
+    //3. prepare UI for results
+
+    //4. serach for recipes
+    await state.search.getResults();
+
+    //5. render results on UI
+    console.log(state.search.result);
+  }
+
+}
+
+document.querySelector('.search').addEventListener('submit', event => {
+  event.preventDefault();
+  cntrlSearch();
+
+
+});
+
 
