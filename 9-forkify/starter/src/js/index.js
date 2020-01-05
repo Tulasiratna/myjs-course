ecf0d87835a10aa53380a2092ee9c3332e9cfea9
Tulasi ratna <ratna.tulasi341@gmail.com>
2020-01-05 18:16:13 +0100

    search view model with clear input fields and clear results

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,27 +1,31 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 const state = {};
 
 const cntrlSearch = async () => {
   // get query from view
-  const query = 'pizza';
+  const query = searchView.getInput();
+  console.log(query);
 
   if (query) {
     //2. new search object and add it to state
     state.search = new Search(query);
 
     //3. prepare UI for results
-
+    searchView.clearInput();
+    searchView.clearResults();
     //4. serach for recipes
     await state.search.getResults();
 
     //5. render results on UI
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result);
   }
 
 }
 
-document.querySelector('.search').addEventListener('submit', event => {
+elements.searchForm.addEventListener('submit', event => {
   event.preventDefault();
   cntrlSearch();
 
